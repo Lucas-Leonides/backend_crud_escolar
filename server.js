4e957439c18fe8041493598c71153d4b837a775f
Lucas-Leonides <142683570+Lucas-Leonides@users.noreply.github.com>
2024-12-02 10:30:00 -0300

    Update server.js
    configurar o IP=(endereço de ip da maquina que ta rodando)

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
+const IP = process.env.IP
 
-app.listen(PORT, () => {
+app.listen(PORT, IP, () => { // Adicione '0.0.0.0' aqui
     console.log(`Servidor rodando na porta ${PORT}`);
 });
